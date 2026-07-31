a9dfbea4b93da1fb1c020454ec4ce27a5aff9859
Alexander Hernandez <alexalh3@uci.edu>
2026-07-31 16:36:51 -0700

    Initial Commit

---
+++ pickup/pickupApp.swift
@@ -0,0 +1,17 @@
+//
+//  pickupApp.swift
+//  pickup
+//
+//  Created by Alexander Hernandez on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct pickupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
